7395b3239c0f6bada6743948a740a33128ea9cbd
tarikworked-max <tarik.worked@gmail.com>
2026-08-25 17:59:28 +0300

    fix: add rewrites to route /saha to frontend vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/saha/:path*',
+          destination: 'https://motofull-zeta.vercel.app/saha/:path*',
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;

11e4b404c826cf300b3cd3311f13294e9504ad33
tarikworked-max <tarik.worked@gmail.com>
2026-08-25 18:00:51 +0300

    feat: add /saha redirect page to frontend panel

---
+++ app/saha/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function SahaPage() {
+  useEffect(() => {
+    window.location.href = 'https://motofull-zeta.vercel.app/saha';
+  }, []);
+
+  return null;
+}
